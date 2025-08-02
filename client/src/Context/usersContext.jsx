ec8559c41970a41d01037c4d61787c1f5ef71046
Samodya <nsamodya@gmail.com>
2025-08-02 15:09:04 +0530

    users view

--- client/src/Context/usersContext.jsx
+++ client/src/Context/usersContext.jsx
@@ -1,7 +1,31 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import apiService from '../utilities/httpservices';
 
-export const getUsersContext = createContext()
+export const UsersContext = createContext()
 
-export const useGetUsers = () => {
-    return useContext(getUsersContext)
+export const useGetUsers = () => useContext(UsersContext)
+
+export const UsersContextProvider = ({children}) => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=> {
+        const fetchUseres = async ()=>{
+            try {
+                const results = await apiService.getData('users');
+                setUsers(results);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchUseres();
+    },[])
+
+    return(
+        <UsersContext.Provider
+            value={users}
+        >
+            {children}
+        </UsersContext.Provider>
+    )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import { AuthContextProvider } from "./Context/AuthContext";
 import AllRoutes from "./Routes";
 import { TasksContextProvider } from "./Context/tasksContext";
+import { UsersContextProvider } from "./Context/usersContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <TasksContextProvider>
+      <UsersContextProvider>
         <AllRoutes />
+      </UsersContextProvider>
       </TasksContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
